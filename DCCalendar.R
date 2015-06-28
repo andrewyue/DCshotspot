ca486c407a9ccc51ac500031487222be26c5d55f
Andrew Yue <andrewyue@users.noreply.github.com>
2015-06-27 22:24:49 -0400

    Update DCCalendar.R
    Added time zone flag to Raw_Data$hour formula.
    Better alignment of the daylight label.

--- DCCalendar.R
+++ DCCalendar.R
@@ -7,7 +7,7 @@ Raw_Data <- read.delim("~/DCCalendar/Raw_Data.txt", header=FALSE)
 Raw_Data[["Rdatetime"]] = as.POSIXct(Raw_Data[["V3"]]*(60*60*24), origin="1899-12-30", tz="GMT")	
 # Prepare new columns in the data frame for data selection - full month name, numeric hour, and 4-digit mmdd
 Raw_Data$month = strftime(Raw_Data$Rdatetime, format="%B")
-Raw_Data$hour = as.numeric(strftime(Raw_Data$Rdatetime, format="%H")) + as.numeric(strftime(Raw_Data$Rdatetime, format="%M"))/60
+Raw_Data$hour = as.numeric(strftime(Raw_Data$Rdatetime, format="%H", tz="GMT")) + as.numeric(strftime(Raw_Data$Rdatetime, format="%M", tz="GMT"))/60
 Raw_Data$monthday = strftime(Raw_Data$Rdatetime, format = "%m%d")
 # Set the dates we want to remove from the raw data
 bad_dates = c("0101", "0703", "0704", "0705", "1231")
@@ -27,7 +27,7 @@ oct = subset(Good_Data, Good_Data$month == "October")
 nov = subset(Good_Data, Good_Data$month == "November")
 dec = subset(Good_Data, Good_Data$month == "December")
 # Prepare the month histograms
-m1=ggplot(jan, aes(hour, fill=cut(..x..,c(7.5,17.25))))+geom_histogram(binwidth=.25, )+guides(fill=FALSE)+scale_x_continuous(breaks=c(0,6,12,18,24))+ggtitle("January")+coord_cartesian(ylim=c(0,200))+xlab("Time of Day")+ylab("Incidents")+annotate("text", x=12.5, y=30, family="Myriad Web Pro", label ="Daylight hours",size=4)+annotate("rect",xmin=7.5, xmax = 17.25, ymin=0, ymax=50,fill="orange",alpha=0.1)+theme(text=element_text(family="Myriad Web Pro", size=24))
+m1=ggplot(jan, aes(hour, fill=cut(..x..,c(7.5,17.25))))+geom_histogram(binwidth=.25, )+guides(fill=FALSE)+scale_x_continuous(breaks=c(0,6,12,18,24))+ggtitle("January")+coord_cartesian(ylim=c(0,200))+xlab("Time of Day")+ylab("Incidents")+annotate("text", x=12.375, y=30, family="Myriad Web Pro", label ="Daylight hours",size=4)+annotate("rect",xmin=7.5, xmax = 17.25, ymin=0, ymax=50,fill="orange",alpha=0.1)+theme(text=element_text(family="Myriad Web Pro", size=24))
 m2=ggplot(feb, aes(hour, fill=cut(..x..,c(7,17.75))))+geom_histogram(binwidth=.25, )+guides(fill=FALSE)+scale_x_continuous(breaks=c(0,6,12,18,24))+ggtitle("February")+coord_cartesian(ylim=c(0,200))+ylab("")+theme(text=element_text(family="Myriad Web Pro", size=24))+xlab("")
 m3=ggplot(mar, aes(hour, fill=cut(..x..,c(7,19))))+geom_histogram(binwidth=.25, )+guides(fill=FALSE)+scale_x_continuous(breaks=c(0,6,12,18,24))+ggtitle("March")+coord_cartesian(ylim=c(0,200))+ylab("")+theme(text=element_text(family="Myriad Web Pro", size=24))+xlab("")
 m4=ggplot(apr, aes(hour, fill=cut(..x..,c(6.5,19.75))))+geom_histogram(binwidth=.25, )+guides(fill=FALSE)+scale_x_continuous(breaks=c(0,6,12,18,24))+ggtitle("April")+coord_cartesian(ylim=c(0,200))+ylab("")+theme(text=element_text(family="Myriad Web Pro", size=24))+xlab("")
